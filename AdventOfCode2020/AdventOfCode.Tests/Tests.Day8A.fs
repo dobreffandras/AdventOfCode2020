c374158d2926e4f3adf4a113863be1494d32002d
András Dobreff <dobreffandras@gmail.com>
2020-12-10 12:11:41 +0100

    Day8 A

---
+++ AdventOfCode2020/AdventOfCode.Tests/Tests.Day8A.fs
@@ -0,0 +1,85 @@
+﻿namespace AdventOfCode.Tests
+
+open AdventOfCode
+open Xunit
+
+module Day8A =
+    
+    [<Fact>]
+    let ``returns 0 for empty program`` () =
+        let result = Day8A.main ""
+        Assert.Equal(0, result)
+
+    [<Fact>]
+    let ``returns 0 for a program having a "nop +0"`` () =
+        let result = Day8A.main "nop +0"
+        Assert.Equal(0, result)
+
+    [<Fact>]
+    let ``returns 1 for a program having an "acc +1"`` () =
+        let result = Day8A.main "acc +1"
+        Assert.Equal(1, result)
+        
+    [<Fact>]
+    let ``returns 2 for a program having an "acc +2"`` () =
+        let result = Day8A.main "acc +2"
+        Assert.Equal(2, result)
+        
+    [<Fact>]
+    let ``returns -2 for a program having an "acc -2"`` () =
+        let result = Day8A.main "acc -2"
+        Assert.Equal(-2, result)
+        
+    [<Fact>]
+    let ``runs for a program having a "nop" and "acc +2"`` () =
+        let result = Day8A.main "nop +0
+        acc +2"
+        Assert.Equal(2, result)
+
+    [<Fact>]
+    let ``return 3 for a program having a "acc +1" and "acc +2"`` () =
+        let result = Day8A.main "acc +1
+        acc +2"
+        Assert.Equal(3, result)
+
+    [<Fact>]
+    let ``return 3 for a program having a "acc +1" a "nop" and "acc +2"`` () =
+        let result = Day8A.main "acc +1
+        nop +0
+        acc +2"
+        Assert.Equal(3, result)
+    
+    [<Fact>]
+    let ``"jmp +2" skips the next instruction"`` () =
+        let result = Day8A.main "jmp +2
+        acc +1
+        acc +2"
+        Assert.Equal(2, result)
+    
+    [<Fact>]
+    let ``"jmp -2" jumps back in program"`` () =
+        let result = Day8A.main "jmp +3
+        acc +5
+        jmp +2
+        jmp -2
+        nop +0"
+        Assert.Equal(5, result)
+
+    [<Fact>]
+    let ``terminates when infinite loop is detected"`` () =
+        let result = Day8A.main "nop +0
+        jmp -1"
+        Assert.Equal(0, result)
+
+    [<Fact>]
+    let ``returns result before right before infinite loop is detected"`` () =
+        let result = Day8A.main "nop +0
+        acc +5
+        jmp -1"
+        Assert.Equal(5, result)
+
+    [<Fact>]
+    let ``returns correctly for sample input"`` () =
+        let result = Day8A.main Inputs.Day8.sampleA
+        Assert.Equal(5, result)
+
---
+++ AdventOfCode2020/AdventOfCode/Day8A.fs
@@ -0,0 +1,85 @@
+﻿namespace AdventOfCode
+
+open System.Management.Instrumentation
+
+module Day8A =
+
+    type Instruction =
+        | NoOp
+        | Acc of int
+        | Jump of int
+        
+    
+    type Program =
+        {
+            instructions :  Instruction [];
+            alreadyRunIndexes : int list;
+            accumulator : int;
+            instructionIndex : int;
+        } with
+      
+        member this.nextInstructionIndex : int =
+            let inst = this.instructions.[this.instructionIndex]
+            match inst with
+                | Jump jump -> this.instructionIndex + jump
+                | _ -> this.instructionIndex + 1
+        
+        member this.calculateNextAccumulator : int =
+            let inst = this.instructions.[this.instructionIndex]
+            match inst with
+                | Acc increment -> this.accumulator + increment
+                | _ -> this.accumulator
+            
+        member this.calculateNextProgram() : Program =
+            let i = this.nextInstructionIndex
+            let acc = this.calculateNextAccumulator
+            let runInstructionIndexes =
+                this.instructionIndex :: this.alreadyRunIndexes
+            {
+                this with
+                    accumulator = acc
+                    instructionIndex = i
+                    alreadyRunIndexes = runInstructionIndexes
+            } 
+        
+    let rec runUntilInfiniteLoop (program : Program) : int =
+        let terminates =
+            program.instructionIndex >= program.instructions.Length
+        let infiniteLoopDetected =
+            List.contains program.instructionIndex program.alreadyRunIndexes
+        if terminates || infiniteLoopDetected then
+            program.accumulator
+        else
+            let nextProgram = program.calculateNextProgram()
+            runUntilInfiniteLoop nextProgram
+
+    module Parser =
+        
+        let parseInstruction (inst: string) (param : string) : Instruction =
+            let p = int param
+            match inst with
+                | "acc" -> Acc p
+                | "jmp" -> Jump p
+                | _ -> NoOp
+        
+        let parseInstructionLine (input : string) : Instruction =
+            let rawInstructionWithParam =
+                Parser.splitBySpace input
+            let rawInstruction = rawInstructionWithParam.[0]
+            let rawParam = rawInstructionWithParam.[1]
+            parseInstruction rawInstruction rawParam
+
+    let main (input : string) : int =
+        let instructions =
+            Parser.parseToRows input
+                |> Seq.map Parser.parseInstructionLine
+                |> Seq.toArray
+        
+        runUntilInfiniteLoop {
+            instructions = instructions;
+            accumulator = 0
+            alreadyRunIndexes = [];
+            instructionIndex = 0;
+        }
+        
+
